45e2f6ce0ae2f52299e74b21165de009b9f6f2e9
bottles10 <henryaovare@gmail.com>
2024-04-21 04:22:33 +0000

    Add show update destroy edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[show edit update destroy]
   def index
     @posts = Post.all.order(created_at: :desc).limit(5)
+  end
+
+  def show
   end
 
   def new 
@@ -17,7 +21,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, alert: "Post successfully destroyed!"
+  end
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:image, :caption)
