a205578e6ae01a721add4bd2ab2f54574b080d2f
bottles10 <henryaovare@gmail.com>
2024-04-23 06:18:35 +0000

    Add ca cert download to render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/e5c15974-91ae-4499-99ba-c5e1cb243f14/cert'
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
